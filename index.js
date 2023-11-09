485b188149aaa192f2bad2425abb55cc8c35ce29
Amin7775 <amin.dpi24@gmail.com>
2023-11-09 17:56:31 +0600

    Api to get single service from db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -50,6 +50,14 @@ async function run() {
         res.send(result)
       })
     //Add Services Related Api - End
+
+    //view single product from db
+    app.get('/services/:id',async(req,res)=> {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await ServiceCollection.findOne(query);
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
